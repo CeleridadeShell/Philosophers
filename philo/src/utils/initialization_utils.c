13a143ced15cbac500513a0ee2da6a1e0e9f8257
m-carecho <milena.c@aluno.ifsp.edu.br>
2023-06-04 23:37:56 +0200

    adjusts

--- philo/src/utils/initialization_utils.c
+++ philo/src/utils/initialization_utils.c
@@ -6,7 +6,7 @@
 /*   By: mcarecho <mcarecho@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 02:24:59 by ccamargo          #+#    #+#             */
-/*   Updated: 2023/06/03 19:19:26 by mcarecho         ###   ########.fr       */
+/*   Updated: 2023/06/04 23:36:26 by mcarecho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,8 @@ void	initialize_philo(t_philo *philo, t_common_data *common, int i)
 	philo->last_meal = 0;
 	philo->left_fork = common->fork[i];
 	philo->right_fork = common->fork[(i + 1) % common->num_of_philos];
+	if((i + 1) % common->num_of_philos == 0) {
+		philo->right_fork = common->fork[i];
+		philo->left_fork = common->fork[(i + 1) % common->num_of_philos];
+	}
 }
